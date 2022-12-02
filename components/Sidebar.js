2d55fde8026085320b907a485161aecec2d80321
alexangria1992 <alex040892@gmail.com>
2022-12-02 21:04:36 +1300

    Sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+function Sidebar() {
+  return (
+    <section className="fixed top-0 z-40 flex flex-col p-4 items-center bg-black w-[90px] h-screen space-y-8">
+      <Image
+        src="https://rb.gy/xkacau"
+        width={56}
+        height={56}
+        objectFit="contain"
+      />
+      <div className="bg-green-300 text-white">hello world</div>
+    </section>
+  );
+}
+
+export default Sidebar;
