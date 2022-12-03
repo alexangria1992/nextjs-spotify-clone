ac1c9b862396592de0f7a2a3bf43b9f0ea0f31e8
alexangria1992 <alex040892@gmail.com>
2022-12-03 19:40:30 +1300

    Next Authentication - authorization

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,11 +3,16 @@ import SpotifyProvider from "next-auth/providers/spotify";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
+    SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      authorization:
+        "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-email,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read",
     }),
     // ...add more providers here
   ],
+  pages: {
+    signIn: "/auth/signin",
+  },
 };
 export default NextAuth(authOptions);
