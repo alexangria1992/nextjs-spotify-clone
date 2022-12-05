4878bac5bee76888159740479d97a50348c5a4bb
alexangria1992 <alex040892@gmail.com>
2022-12-06 12:35:57 +1300

    Body - session/accessToken

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import { useSession } from "next-auth/react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search";
 
 function Body() {
+  const { data: session } = useSession();
+
   const [search, setSearch] = useState("");
+  const { accessToken } = session;
   const [searchResults, setSearchResults] = useState([]);
+  const [newReleases, setNewReleases] = useState([]);
+
+  useEffect(() => {
+    if (!accessToken) return false;
+  }, []);
+
+  console.log(accessToken);
 
   return (
     <section className=" ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5 ">
