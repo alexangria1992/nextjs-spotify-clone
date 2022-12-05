680541ae34cca79460c4b56e0a9589a672b007ff
alexangria1992 <alex040892@gmail.com>
2022-12-05 16:07:38 +1300

    Login ServerSideProps

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
+import { getProviders, useSession } from "next-auth/react";
 
-function Signin() {
+function Signin({ providers }) {
+  const { data: session } = useSession();
   return (
     <div>
       <Head>
@@ -18,8 +20,25 @@ function Signin() {
         alt=""
         priority
       />
+      {Object.values(providers).map((provider) => (
+        <div key={provider.id}>
+          <button
+            className="text-white py-4 px-6 rounded-full bg-[#1db954] transition duration-300 ease-out border border-transparent uppercase font-bold text-xs md:text-base tracking-wider hover:scale-105 hover:bg-[#0db146]"
+            onClick={() => signIn(provider.id)}
+          >
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Signin;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
