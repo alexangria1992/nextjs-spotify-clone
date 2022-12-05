3417f6b53742424d84338af588b31c22a092733d
alexangria1992 <alex040892@gmail.com>
2022-12-05 15:55:15 +1300

    Login - auth

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Head from "next/head";
+import Image from "next/image";
 
-function signin() {
-  return <div>signin</div>;
+function Signin() {
+  return (
+    <div>
+      <Head>
+        <title>Login - Spotify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Image
+        src="https://rb.gy/y9mwtb"
+        width={250}
+        height={600}
+        className="object-contain animate-pulse"
+        alt=""
+        priority
+      />
+    </div>
+  );
 }
 
-export default signin;
+export default Signin;
